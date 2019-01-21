6b7f6f68a313958882d5260929fb4894778859c6
1169794338@qq.com <Liziyi521521>
2019-01-21 13:06:45 +0800

    init

---
+++ src/pages/Property/List/TableForm.js
@@ -0,0 +1,319 @@
+import React, { PureComponent, Fragment } from 'react';
+import { Table, Button, Input, message, Popconfirm, Divider, Select } from 'antd';
+import isEqual from 'lodash/isEqual';
+import styles from './style.less';
+
+const propertyType = [
+  {
+    key: 'Integer',
+    label: 'Integer',
+  },
+  {
+    key: 'Long',
+    label: 'Long',
+  },
+  {
+    key: 'Double',
+    label: 'Double',
+  },
+  {
+    key: 'Float',
+    label: 'Float',
+  },
+  {
+    key: 'BigDecimal',
+    label: 'BigDecimal',
+  },
+  {
+    key: 'Boolean',
+    label: 'Boolean',
+  },
+  {
+    key: 'Date',
+    label: 'Date',
+  },
+  {
+    key: 'String',
+    label: 'String',
+  },
+];
+
+class TableForm extends PureComponent {
+  index = 0;
+
+  cacheOriginData = {};
+
+  constructor(props) {
+    super(props);
+    console.log(props.value);
+    this.state = {
+      data: props.value,
+      loading: false,
+      value: props.value,
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, preState) {
+    if (isEqual(nextProps.value, preState.value)) {
+      return null;
+    }
+    return {
+      data: nextProps.value,
+      value: nextProps.value,
+    };
+  }
+
+  getRowByKey(key, newData) {
+    const { data } = this.state;
+    return (newData || data).filter(item => item.key === key)[0];
+  }
+
+  toggleEditable = (e, key) => {
+    e.preventDefault();
+    const { data } = this.state;
+    const newData = data.map(item => ({ ...item }));
+    const target = this.getRowByKey(key, newData);
+    if (target) {
+      // 进入编辑状态时保存原始数据
+      if (!target.editable) {
+        this.cacheOriginData[key] = { ...target };
+      }
+      target.editable = !target.editable;
+      this.setState({ data: newData });
+    }
+  };
+
+  newMember = () => {
+    const { data } = this.state;
+    const newData = data.map(item => ({ ...item }));
+    newData.push({
+      key: `${data.length + 1}`,
+      name: '',
+      type: '',
+      memo: '',
+      editable: true,
+      isNew: true,
+    });
+    this.index += 1;
+    this.setState({ data: newData });
+  };
+
+  remove(key) {
+    const { data } = this.state;
+    const { onChange } = this.props;
+    // const newData = data.filter(item => item.key !== key);
+    const target = data.filter(item => item.key === key);
+    // this.setState({ data: newData });
+    onChange(target, 'remove');
+  }
+
+  handleKeyPress(e, key) {
+    if (e.key === 'Enter') {
+      this.saveRow(e, key);
+    }
+  }
+
+  handleFieldChange(e, fieldName, key) {
+    const { data } = this.state;
+    const newData = data.map(item => ({ ...item }));
+    const target = this.getRowByKey(key, newData);
+    if (target) {
+      if (fieldName === 'type') {
+        target[fieldName] = e;
+      } else {
+        target[fieldName] = e.target.value;
+      }
+      this.setState({ data: newData });
+    }
+  }
+
+  saveRow(e, key) {
+    e.persist();
+    this.setState({
+      loading: true,
+    });
+    setTimeout(() => {
+      if (this.clickedCancel) {
+        this.clickedCancel = false;
+        return;
+      }
+      const target = this.getRowByKey(key) || {};
+      if (!target.type || !target.name) {
+        message.error('请填写完整的属性信息。');
+        e.target.focus();
+
+        this.setState({
+          loading: false,
+        });
+        return;
+      }
+      delete target.isNew;
+      this.toggleEditable(e, key);
+      // const { data } = this.state;
+      const { onChange } = this.props;
+      // onChange(data);
+      onChange(target, 'save');
+      this.setState({
+        loading: false,
+      });
+    }, 500);
+  }
+
+  cancel(e, key) {
+    this.clickedCancel = true;
+    e.preventDefault();
+    const { data } = this.state;
+    const newData = data.map(item => ({ ...item }));
+    const target = this.getRowByKey(key, newData);
+    if (this.cacheOriginData[key]) {
+      Object.assign(target, this.cacheOriginData[key]);
+      delete this.cacheOriginData[key];
+    }
+    target.editable = false;
+    this.setState({ data: newData });
+    this.clickedCancel = false;
+  }
+
+  render() {
+    const columns = [
+      {
+        title: '序号',
+        dataIndex: 'key',
+        key: 'key',
+        align: 'center',
+        width: 80,
+      },
+      {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name',
+        align: 'center',
+        width: '20%',
+        render: (text, record) => {
+          if (record.editable) {
+            return (
+              <Input
+                value={text}
+                autoFocus
+                onChange={e => this.handleFieldChange(e, 'name', record.key)}
+                onKeyPress={e => this.handleKeyPress(e, record.key)}
+                placeholder="属性名称"
+              />
+            );
+          }
+          return text;
+        },
+      },
+      {
+        title: '类型',
+        dataIndex: 'type',
+        key: 'type',
+        align: 'center',
+        width: '20%',
+        render: (text, record) => {
+          if (record.editable) {
+            return (
+              <Select
+                value={text}
+                style={{ width: '100%' }}
+                onChange={e => this.handleFieldChange(e, 'type', record.key)}
+                onKeyPress={e => this.handleKeyPress(e, record.key)}
+                placeholder="类型"
+              >
+                {propertyType.map(item => (
+                  <Select.Option key={item.key}>{item.label}</Select.Option>
+                ))}
+              </Select>
+            );
+          }
+          return text;
+        },
+      },
+      {
+        title: '描述',
+        dataIndex: 'memo',
+        key: 'memo',
+        width: '40%',
+        render: (text, record) => {
+          if (record.editable) {
+            return (
+              <Input
+                value={text}
+                onChange={e => this.handleFieldChange(e, 'memo', record.key)}
+                onKeyPress={e => this.handleKeyPress(e, record.key)}
+                placeholder="描述"
+              />
+            );
+          }
+          return text;
+        },
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => {
+          const { loading } = this.state;
+          if (!!record.editable && loading) {
+            return null;
+          }
+          if (record.editable) {
+            if (record.isNew) {
+              return (
+                <span>
+                  <a onClick={e => this.saveRow(e, record.key)}>添加</a>
+                  <Divider type="vertical" />
+                  <Popconfirm title="是否要删除此行？" onConfirm={() => this.remove(record.key)}>
+                    <a>删除</a>
+                  </Popconfirm>
+                </span>
+              );
+            }
+            return (
+              <span>
+                <a onClick={e => this.saveRow(e, record.key)}>保存</a>
+                <Divider type="vertical" />
+                <a onClick={e => this.cancel(e, record.key)}>取消</a>
+              </span>
+            );
+          }
+          return (
+            <span>
+              <a onClick={e => this.toggleEditable(e, record.key)}>编辑</a>
+              <Divider type="vertical" />
+              <Popconfirm title="是否要删除此行？" onConfirm={() => this.remove(record.key)}>
+                <a>删除</a>
+              </Popconfirm>
+            </span>
+          );
+        },
+      },
+    ];
+
+    const { loading, data } = this.state;
+
+    return (
+      <Fragment>
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={data}
+          rowKey={row => row.id}
+          pagination={false}
+          size="small"
+          bordered
+          rowClassName={record => (record.editable ? styles.editable : '')}
+        />
+        <Button
+          style={{ width: '100%', marginTop: 16, marginBottom: 8 }}
+          type="dashed"
+          onClick={this.newMember}
+          icon="plus"
+        >
+          新增属性
+        </Button>
+      </Fragment>
+    );
+  }
+}
+
+export default TableForm;
